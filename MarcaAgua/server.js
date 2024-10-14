c1b4e0276aa798f8dc7b818fd45d2e95c47a7c70
SamVargas <samuel.vargas02@uptc.edu.co>
2024-10-14 10:36:55 -0500

    update ports

--- MarcaAgua/server.js
+++ MarcaAgua/server.js
@@ -5,7 +5,7 @@ const sharp = require('sharp');
 const cors = require('cors');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.use(cors());
 app.use(fileUpload());
